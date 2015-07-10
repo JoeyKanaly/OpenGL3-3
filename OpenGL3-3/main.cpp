7e82ca0e4d3576574486a654bdcdb22483821f8b
Joey <me@joeykanaly.com>
2015-07-10 01:10:29 -0500

    Third tutorial completed. This also includes a way to reload the shaders however it could be simply improved with the use of variables for the files.

--- OpenGL3-3/main.cpp
+++ OpenGL3-3/main.cpp
@@ -5,11 +5,15 @@
 #include "Tools.h"
 
 GLFWwindow* window;
+GLuint program;
+
 
 void keyboard(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
+	if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+		program = compileShaders(".\\shaders\\tut3.vert.glsl", ".\\shaders\\tut3.frag.glsl");
 }
 
 int initWindow()
@@ -60,10 +64,10 @@ int main()
 	glGenBuffers(1, &EBO);
 
 	GLfloat verts[] = {
-		0.5f,  0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		-0.5f, 0.5f, 0.0f
+		 0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.6f,
+		 0.5f, -0.5f, 0.0f, 0.2f, 0.9f, 0.1f,
+		-0.5f, -0.5f, 0.0f, 0.1f, 0.2f, 0.3f,
+		-0.5f,  0.5f, 0.0f, 0.4f, 0.5f, 0.6f
 	};
 	
 	GLuint indicies[] = {
@@ -71,27 +75,36 @@ int main()
 		1, 2, 3
 	};
 
+	program = compileShaders(".\\shaders\\tut3.vert.glsl", ".\\shaders\\tut3.frag.glsl");
+
 	glBindVertexArray(VAO);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+		glEnableVertexAttribArray(1);
 	glBindVertexArray(0);
 	
-	GLuint program = compileShaders(".\\shaders\\tut2.vert.glsl", ".\\shaders\\tut2.frag.glsl");
-	
-	//Wireframe mode
+	//GLint nrAttributes;
+	//glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	//std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+	//Wire frame mode
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
+		GLfloat timeValue = glfwGetTime();
+		GLfloat greenValue = (sin(timeValue) / 2) + .5;
+		GLint vertexColorLocation = glGetUniformLocation(program, "ourColor");
 		glUseProgram(program);
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 		glBindVertexArray(VAO);
+
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
