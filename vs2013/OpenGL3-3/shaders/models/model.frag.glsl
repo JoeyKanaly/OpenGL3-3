f6e506f2207c52d498a98a7604c6cb67ec8a10a6
Joey <me@joeykanaly.com>
2015-08-01 22:45:20 -0500

    Added some of the textures to the dice.

--- vs2013/OpenGL3-3/shaders/models/model.frag.glsl
+++ vs2013/OpenGL3-3/shaders/models/model.frag.glsl
@@ -11,8 +11,8 @@ out vec4 color;
 
 void main()
 {
-	//color = vec4(texture(texture_specular1, TexCoords));
+	color = vec4(texture(texture_specular1, TexCoords));
 	//color = vec4(sin(time), 1.0f, 0.4f, 1.0f);
 	//color = vec4(1.0f, 0.5f, 0.3, 1.0f);
-	color = vec4(inColor, 1.0f);
+	//color = vec4(inColor, 1.0f);
 }
